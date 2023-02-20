cc32cd4e41a5b692f39a79f51296dd56159b7949
YaaseenK <yaaseenkhan02@gmail.com>
2023-02-19 19:40:23 -0500

    added styling to the nav bar and added logos

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { HashLink } from 'react-router-hash-link';
+import navIcon1 from '../../assets/img/nav/nav-icon1.svg';
+import navIcon2 from '../../assets/img/nav/nav-icon2.svg';
+import navIcon3 from '../../assets/img/nav/nav-icon3.svg';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -27,47 +30,51 @@ export const NavBar = () => {
 
     return (
       <Router>
-          <Navbar className={scrolled? "scrolled": ""} justify variant="tabs" defaultActiveKey="/home">
-            <Container>
-              
-              <Navbar.Brand href="/">
-                <img src='{logo}' alt="Logo" />
-              </Navbar.Brand>
-
-              <Navbar.Toggle aria-controls="basic-navbar-nav">
-                <span className="navbar-toggler-icon"></span>
-              </Navbar.Toggle>
-
-              <Navbar.Collapse id="basic-navbar-nav">
-                
+          <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+          <Container>
+            <Navbar.Brand href="/">
+              <img src='' alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+          
+          <Navbar.Collapse id="basic-navbar-nav">
                 <Nav classname='ms-auto'>  
                   <Nav.Link 
                     href="#home" 
-                    className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('home')}
+                    className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} 
+                    onClick={() => updateActiveLink('home')}
                     >Home
                   </Nav.Link>
 
                   <Nav.Link 
-                    href='#skills'
-                    classname={activeLink=== 'skills' ? 'active-navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('')}
-                    >Portfolio
+                    href="#skills" 
+                    className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} 
+                    onClick={() => updateActiveLink('skills')}
+                    >Skills
                   </Nav.Link>
 
                   <Nav.Link 
-                      href='#about'
-                      classname={activeLink=== 'about' ? 'active-navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('')}
-                      >About Me
+                    href="#projects" 
+                    className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} 
+                    onClick={() => updateActiveLink('projects')}
+                    >Projects
                   </Nav.Link>
 
                   <Nav.Link 
-                      classname={activeLink=== 'contact' ? 'active-navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('')}
-                      href='#contact'>Contact Me
+                    href="#Contact" 
+                    className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} 
+                    onClick={() => updateActiveLink('contact')}
+                    >Contact Me
                   </Nav.Link>
+                </Nav>
+                
                   <span className='navbar-text'>
                     <div className='social-icons'>
-                      <Nav.Link href="#"><img src='' alt='' /></Nav.Link>
-                      <Nav.Link href="#"><img src='' alt='' /></Nav.Link>
-                      <Nav.Link href="#"><img src='' alt='' /></Nav.Link>
+                      <a href="#"><img src={navIcon1} alt="" /></a>
+                      <a href="#"><img src={navIcon2} alt="" /></a>
+                      <a href="#"><img src={navIcon3} alt="" /></a>
                     </div>
                     <HashLink to='#connect'>
                       <button className="vvd">
@@ -76,7 +83,7 @@ export const NavBar = () => {
                     </HashLink> 
                   </span>
                   
-                </Nav>
+              
               </Navbar.Collapse>
             </Container>
           </Navbar>
